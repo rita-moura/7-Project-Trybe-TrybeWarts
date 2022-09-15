ae0f4c372b76b12f64dbc48350a3935b6fc9ef92
Rita Moura <ritamoura520@gmail.com>
2022-09-15 13:45:08 -0300

    Iniciei o requisito 21

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ const getButtonFormsSubmit = document.querySelector('#submit-btn');
 const getInputChackbox = document.querySelector('#agreement');
 const getTextarea = document.querySelector('#textarea');
 const getCounter = document.querySelector('#counter');
-const maxLength = 500;
+const getNome = document.querySelector('#input-name');
+const getSobrenome = document.querySelector('#input-lastname');
+const getEmail = document.querySelector('#inputemail');
+const getOl = document.querySelector('#ol-dados-forms');
+const getHome = document.querySelector('#label-casa');
+const getFamily = document.querySelector('#label-family');
+const getContent = document.querySelector('#label-content');
 const getButtonLogin = document.querySelector('.submit');
 
 function login() {
@@ -20,9 +26,31 @@ function submitForms() {
 }
 
 function counterTextarea() {
+  const maxLength = 500;
   const countDecrement = maxLength - getTextarea.value.length;
   getCounter.textContent = countDecrement;
 }
+
+function getForm() {
+  const liDados = document.createElement('li');
+  const textNome = document.createElement('p');
+  const textEmail = document.createElement('p');
+  const textHomeFamily = document.createElement('p');
+  const textContent = document.createElement('p');
+  const textAreaDados = document.createElement('p');
+  textNome.innerText = `Nome: ${getNome.value} ${getSobrenome.value}`;
+  textEmail.innerText = `Email: ${getEmail.value}`;
+  textHomeFamily.innerText = `Casa: ${getHome.value}, Família: ${getFamily.value}`;
+  textContent.innerText = `Matérias: ${getContent.value}`;
+  textAreaDados.innerText = `Observações: ${getTextarea.value}`;
+  liDados.appendChild(textNome);
+  liDados.appendChild(textEmail);
+  liDados.appendChild(textHomeFamily);
+  liDados.appendChild(textContent);
+  liDados.appendChild(textAreaDados);
+  getOl.appendChild(liDados);
+}
+console.log(getForm());
 
 getButtonLogin.addEventListener('click', login);
 getInputChackbox.addEventListener('click', submitForms);
