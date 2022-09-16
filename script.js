5027d5f6c7f5f5aa0277ce35ea2577c98297e46e
Rita Moura <ritamoura520@gmail.com>
2022-09-16 11:54:21 -0300

    tentativa de fazer 21 falhou novamente

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const getTextarea = document.querySelector('#textarea');
 const getCounter = document.querySelector('#counter');
 const getNome = document.querySelector('#input-name');
 const getSobrenome = document.querySelector('#input-lastname');
-const getEmail = document.querySelector('#inputemail');
+const getEmail = document.querySelector('#input-email');
 const getOl = document.querySelector('#ol-dados-forms');
 const getHome = document.querySelector('#label-casa');
 const getFamily = document.querySelector('#label-family');
@@ -31,30 +31,30 @@ function counterTextarea() {
   getCounter.textContent = countDecrement;
 }
 
-function getForm() {
-  const liDados = document.createElement('li');
-  const textNome = document.createElement('p');
-  const textEmail = document.createElement('p');
-  const textHomeFamily = document.createElement('p');
-  const textContent = document.createElement('p');
-  const textAreaDados = document.createElement('p');
+function getForm(event) {
+  event.preventDefault();
+  const textNome = document.createElement('li');
+  const textEmail = document.createElement('li');
+  const textHomeFamily = document.createElement('li');
+  const textContent = document.createElement('li');
+  const textAreaDados = document.createElement('li');
   textNome.innerText = `Nome: ${getNome.value} ${getSobrenome.value}`;
   textEmail.innerText = `Email: ${getEmail.value}`;
-  textHomeFamily.innerText = `Casa: ${getHome.value}, Família: ${getFamily.value}`;
+  console.log(textEmail);
+  textHomeFamily.innerText = `Casa: ${getHome.value} Família: ${getFamily.value}`;
   textContent.innerText = `Matérias: ${getContent.value}`;
   textAreaDados.innerText = `Observações: ${getTextarea.value}`;
-  liDados.appendChild(textNome);
-  liDados.appendChild(textEmail);
-  liDados.appendChild(textHomeFamily);
-  liDados.appendChild(textContent);
-  liDados.appendChild(textAreaDados);
-  getOl.appendChild(liDados);
+  getOl.appendChild(textNome);
+  getOl.appendChild(textEmail);
+  getOl.appendChild(textHomeFamily);
+  getOl.appendChild(textContent);
+  getOl.appendChild(textAreaDados);
 }
-getForm();
 
 getButtonLogin.addEventListener('click', login);
 getInputChackbox.addEventListener('click', submitForms);
 getTextarea.addEventListener('input', counterTextarea);
+getButtonFormsSubmit.addEventListener('click', getForm);
 
 window.onload = function disableButton() {
   getButtonFormsSubmit.disabled = true;
